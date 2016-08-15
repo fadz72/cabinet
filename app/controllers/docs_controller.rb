c9d1e78c5b32d96161d292381b35757f6185d9eb
Fadz <fadz@Fadzs-MacBook-Pro.local>
2016-08-15 23:45:30 +0800

    (edit and destroy doc

--- app/controllers/docs_controller.rb
+++ app/controllers/docs_controller.rb
@@ -23,10 +23,22 @@ class DocsController < ApplicationController
 		end
 	end
 
+	def edit
+		#has been dealt with from the before_action
+	end
+
 	def update
+		if @doc.update(doc_params)
+			redirect_to@doc
+		else
+			render "edit"
+		end
+
 	end
 
 	def destroy
+		@doc.destroy
+		redirect_to docs_path
 	end
 
 	private
